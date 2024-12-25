5b4b6f61845540a0f521545c060aa340d129d9e4
Ayush <ayushchahat110903@gmail.com>
2024-12-25 17:04:07 +0530

    Done some changes in manual automation

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ io.on('connection', (socket) => {
     };
 
     socket.emit('sensor-data', sampleData);
-  }, 6000);
+  }, 10000);
 
   // Clean up on disconnect
   socket.on('disconnect', () => {
